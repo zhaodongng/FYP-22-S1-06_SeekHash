c27b98966c0a60b21adf571f4206b6d95f3d832c
JaySee95 <49748832+JaySee95@users.noreply.github.com>
2022-03-23 20:02:33 +0800

    Update index_template.php

--- web-dev/index_template.php
+++ web-dev/index_template.php
@@ -17,14 +17,15 @@
                     <nav class="mt-3">
                             <ul class ="nav__links">
                                 <li><a href="home.php">Home</a></li>
-                                <li><a href="#">Report</a></li>
+                                <li><a href="report_page.php">Report</a></li>
                                 <li><a href="history.php">History</a></li>
                                 <li><a href="program_page.php">Program</a></li>
-                                <li><a href="login_page.php"></a><button type="button" class="logInBtn">
+                                <li><button type="button" onclick="location.href='login_page.php';" class="logInBtn">
                                 <svg style="color: #F2F7FF;" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                                 </svg>&nbsp;Log In</button></li>
+                                </button>
                             </ul>
                         </nav>
                     </div>
