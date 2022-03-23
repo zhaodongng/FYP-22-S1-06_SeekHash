6a5a0f883ef754f42b987b102b41e89288605e0f
JaySee95 <49748832+JaySee95@users.noreply.github.com>
2022-03-23 18:12:43 +0800

    Update processsignup.php

--- web-dev/processsignup.php
+++ web-dev/processsignup.php
@@ -1,48 +1,76 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>SeekHash</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel= "stylesheet" href="styles/index_template.css" type="text/css"/> 
-    </head>
-    <body>
-		<?php
-            // connect to sql databases
-			$servername = "localhost";
-            $username = "root";
-            $password = "root";
-            $dbname = "seekhash_db";
-    
-            try{
-                $conn = mysqli_connect($servername, $username, $password, $dbname);
-                //echo "<p>Connection to seekhash_db successful</p>\n";
-            }
-            catch (mysqli_sql_exception $e)
-            {
-                die("Connection to seekhash_db failed: " . mysqli_connect_errno() . " - " . 
-                    mysqli_connect_error());
-            }
+<?php
 
-            $sql = "INSERT INTO seekhash_db.user_info(user_id, name, password, email) 
-                    VALUES (NULL,'{$_POST["signup_username"]}','{$_POST["signup_password"]}','{$_POST["signup_email"]}')";
+    $signup_user= $_POST["signup_username"];
+    $signup_pass= $_POST["signup_password"];
+    $signupcfm_pass= $_POST["signup_confirmpassword"];
+    $signup_email = $_POST["signup_email"];
 
-            try{
-                $conn = mysqli_query($conn, $sql);
-                echo "<p>INSERT row to seekhash_db.user_info successfully</p>\n";
-            }
-            catch (mysqli_sql_exception $e)
-            {
-                die("Insert row to seekhash_db.user_info failed: " . mysqli_connect_errno() . " - " . 
-                    mysqli_connect_error());
-            }
-
-            include "home.html";
-            //echo "<p>New user has been signed up.</p>";
-
+    function checkUserExists($signup_user){
+        $verify = false;
+        if (!empty($signup_user)){ $verify = true; }
+        else { $verify = false; }
+        if ($verify == true){
+            $conn = mysqli_connect("localhost", "root", "", "seekhash_db");
+            if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+            $sql = "SELECT * FROM seekhash_db.user_info WHERE name = '$signup_user'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0){
+                echo "Username taken";
+                $verify = false; }
             mysqli_close($conn);
-		?>
-    </body>
-</html>
+        }
+        return $verify;
+    }
+
+    function checkPass($signup_pass, $signupcfm_pass){
+        $verify = false;
+        if (!empty($signup_pass) and (!empty($signupcfm_pass))){ $verify = true; }
+        else { $verify = false; }
+        if ($verify == true){
+            if ($signup_pass != $signupcfm_pass){
+                $verify = false;
+                echo "Passwords does not match";
+            }
+        }    
+        return $verify;
+    }
+
+    function checkEmail($signup_email){
+        $verify = false;
+        if (filter_var($signup_email, FILTER_VALIDATE_EMAIL)) { $verify = true; }
+        else { echo "invalid email"; }
+        if ($verify = true) {
+            $conn = mysqli_connect("localhost", "root", "", "seekhash_db");
+            if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+            $sql = "SELECT * FROM seekhash_db.user_info WHERE email = '$signup_email'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0){
+                echo "Email taken";
+                $verify = false; }
+            mysqli_close($conn);
+        }
+        return $verify;
+    }
+
+    function addUser($signup_user, $signup_pass, $signup_email){
+        $conn = mysqli_connect("localhost", "root", "", "seekhash_db");
+        if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+        $sql = "INSERT INTO seekhash_db.user_info(name, password, email) VALUES ('$signup_user', '$signup_pass', '$signup_email')";
+        if($conn->query($sql)){
+            echo "results inserted!";
+        }
+        mysqli_close($conn);
+    }
+
+    $username_check = checkUserExists($signup_user);
+    if ($username_check == 1){
+        $password_check = checkPass($signup_pass, $signupcfm_pass);
+        if ($password_check == 1){
+            $email_check = checkEmail($signup_email);
+            if ($email_check == 1) {
+                addUser($signup_user, $signup_pass, $signup_email);
+            }
+        }
+    }
+    include 'home.php';
+    #header(location:"home.php"); 
