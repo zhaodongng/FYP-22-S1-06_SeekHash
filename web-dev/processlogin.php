c8b7eba98784654dceab0a208527510b07296469
zhaodongng <zhaodongng@gmail.com>
2022-03-24 12:23:30 +0800

    Update processlogin.php

--- web-dev/processlogin.php
+++ web-dev/processlogin.php
@@ -50,7 +50,7 @@
     if ($verify == 1){
         $verify = verify_access($login_user, $login_pass);
         echo "im in";
-        include "home_after_login.php";
+        include "home_after_logIn.php";
     } else {
         echo "Invalid username or password";
     }
