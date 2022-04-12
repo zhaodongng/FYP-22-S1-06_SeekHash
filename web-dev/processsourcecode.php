c0ebc96415fe071ebb1bdc75f32daacbdcf057dd
zhaodongng <91584796+zhaodongng@users.noreply.github.com>
2022-04-12 18:33:21 +0800

    Update database structure for processsourcecode.php

--- web-dev/processsourcecode.php
+++ web-dev/processsourcecode.php
@@ -15,8 +15,31 @@
 
             // Execute the SeekHash python program.
             $output = shell_exec("/usr/local/bin/python3.8 seekhash_v3.2.py $sourceCodeFile 2>&1");
-            echo $output;
+            //echo $output;
 
+            // Convert output string into an array
+            $output_array = explode(";", $output);
+            array_pop($output_array);    // Remove empty element
+            echo "<pre>";
+            print_r($output_array);
+            echo "</pre>";
+
+            $file_read_in = $output_array[0];
+            $programming_language_detected = $output_array[1];
+            $platforms = $output_array[2];
+            $probabilities = $output_array[3];
+            $hashes_found_at = $output_array[4];
+
+            // Insert elements of output array into seekhashdb.file_info table
+            /*
+            $conn = mysqli_connect("localhost", "root", "root", "seekhash_db");
+            if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+            $sql = "INSERT INTO seekhash_db.file_info(name, password, email) VALUES ('$signup_user', '$signup_pass', '$signup_email')";
+            if($conn->query($sql)){
+                echo "results inserted!";
+            }
+            mysqli_close($conn);
+            */
             include "report_page.php";
             
         ?>
