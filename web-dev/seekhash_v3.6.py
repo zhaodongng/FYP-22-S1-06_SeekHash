dce9253235c40a558e27895d02ef5520b243917d
JaySee95 <49748832+JaySee95@users.noreply.github.com>
2022-05-04 12:44:16 +0800

    re-adjusted output for ";"

--- web-dev/seekhash_v3.6.py
+++ web-dev/seekhash_v3.6.py
@@ -513,13 +513,8 @@ def main():
             if Dict_key in hash_found and Dict_key not in found_array:
                 found_array.append(Dict_key)
                 outputTxt += "{}-{}bits".format(Dict_key.upper(), hashLengthDict[Dict_key])
-                """
-                for past in past_Attacks:
-                    if Dict_key.lower() == past:
-                        outputTxt += past_Attacks[Dict_key.lower()]
-                        past_attacks_done.append(Dict_key.lower())
-                """
-                outputTxt += ","
+
+    outputTxt += ","
 
     #-7- Find digital signature used in the programming language
     digitalSignature = find_digsig(language, contents)
