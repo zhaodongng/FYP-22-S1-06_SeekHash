9efa72c4aff430c9dcd9a7c867acfbd57d012dc8
Khin Chaw Thazin <jenolovekc37@gmail.com>
2022-05-13 13:03:55 +0630

    block address transaction program page is updated.but not finished yet

--- web-dev/blockhash_page.php
+++ web-dev/blockhash_page.php
@@ -27,7 +27,28 @@ else
     </head>
     <body>
         <!-- BlockHash Page Contents -->
-        <section class="blockhashprogram">
+        <h3 class="upload_h3">Upload Block Hash</h3>
+        <div class="blockhashForm">
+            <form action="processblockhash.php" method="post">
+                <h5>Select blockhash option:</h5>
+                <div class="row">
+                    <div class="col-2 col-md-5"></div>
+                    <div class="col-8 col-md-4 text-start">
+                        <input type="radio" id="block" name="blockhash_option" value="Block" />
+                        <label for="block">Block</label></br>
+                        <input type="radio" id="address" name="blockhash_option" value="Address" />
+                        <label for="address">Address</label></br>
+                        <input type="radio" id="transaction" name="blockhash_option" value="Transaction" />
+                        <label for="transaction">Transaction</label><br />
+                    </div>
+                    <div class="col-2 col-md-3"></div>
+                </div>
+                <input type="text" name="blockhash" onfocus="this.value = ''" value="Type your blockhash"/>&nbsp;&nbsp;
+                <input type="submit" />
+            </form>
+        </div>
+        <!-- BlockHash Page Contents -->
+        <!-- <section class="blockhashprogram">
             <div class="blockhashbox">
                 <form action="processblockhash.php" method="post">
                     <h2 style="color:aliceblue;"><b>Upload Block Hash</b></h2>
@@ -42,13 +63,8 @@ else
                     <input type="submit" />
                 </form>
             </div>
-        </section>
+        </section> -->
         
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
 </html>
 
