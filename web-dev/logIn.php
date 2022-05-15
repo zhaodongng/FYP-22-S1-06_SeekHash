645100c05ce87c0ae669ca284b4815b53f92f5e2
Khin Chaw Thazin <jenolovekc37@gmail.com>
2022-05-15 16:40:07 +0630

    logIn page is updated.

--- web-dev/logIn.php
+++ web-dev/logIn.php
@@ -1,38 +1,45 @@
+<!DOCTYPE html>
 <html>
-    <header>
-    <link rel= "stylesheet" href="styles/logIn_template.css" type="text/css"/> 
-    </header>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>SeekHash - LogIn Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel= "stylesheet" href="styles/logIn_template.css" type="text/css"/> 
+    </head>
     <body>
-        <div class="cancelbtn">
-            <a href="home.php">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#854DFF" class="bi bi-x" viewBox="0 0 16 16">
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-            </a>
-        </div>
-        <div class="container">
-            <div class="title">LogIn Form</div>
-            <div class="content">
-                <form method='POST' action='processlogin.php'>
-                    <div class="user-details">
-                        <div class="input-box">
-                            <span class="details">Name</span>
-                            <input type="text"  name='name' autocomplete="off" placeholder="Enter your name" required>
-                        </div>
-                        <div class="input-box">
-                            <span class="details">Password</span>
-                            <input type="password" name='pwd' autocomplete="off" placeholder="Enter your password" required>
-                        </div>
-                    </div>
-                    <div class="button">
-                        <input type="submit" value="LogIn" name='checkbutton'>
-                    </div>
-                </form>
+        <!-- LogIn Page Contents -->
+        <!-- x button -->
+        <div class="row">
+            <div class="col-5 col-md-9">
+                <div class="cancelbtn">
+                    <a href="home.php">
+                        <img src="img/xmark-solid.svg" />
+                    </a>
+                </div>
             </div>
+            <div class="col-7 col-md-3"></div>
         </div>
 
+        <div class="loginbox">
+            <h1>LogIn</h1>
+            <form action="processlogin.php" method="post">
+                <input type="text" name="name" onfocus="this.value = ''"  autocomplete="off" placeholder="User Name" required/><br />
+                <input type="password" name="pwd" onfocus="this.value = ''" autocomplete="off" placeholder="Password" required/><br />
+                <input type="submit" value="LogIn" name='checkbutton' class="checkbutton"/>
+            </form>
+        </div>
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
 </html>
-<?php
-    
-?>
